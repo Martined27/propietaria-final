794ab97c1045a262bfd5341dbd8054fa3eb593ff
martined27 <edmundodeleon@gmail.com>
2016-07-09 14:53:43 -0400

    Add CaculadoraPrestamos
    Se agregaron view de calcualdora con ajax
    Se tradujeron encabezados
    Se agregaron links a otros cruds
    Se agrego logo.

--- SisPrestamo/Controllers/CalculadoraController.cs
+++ SisPrestamo/Controllers/CalculadoraController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Globalization;
 
 namespace SisPrestamo.Controllers
 {
@@ -15,8 +16,32 @@ namespace SisPrestamo.Controllers
         {
             
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+
             return View();
         }
 
+        public JsonResult CalcularPrestamo(double Monto, double Tasa, int Plazo)
+        {
+            int p = Plazo * -1;
+            Tasa = Tasa/100;
+            double b = (1 + Tasa);
+            double c = (1 - Math.Pow(b, p));
+           
+            double res_cuotafija = (Tasa * Monto)/c;
+            double interes_anual = (res_cuotafija * 12);
+            double res_total= res_cuotafija * Plazo;
+            double costo_fin = res_total - Monto;
+
+            CultureInfo rdCulture = new CultureInfo("es-DO");
+
+            return Json(new
+            {
+                cuota_fija = string.Format("RD {0:C}", res_cuotafija),
+                interes = string.Format("RD {0:C}", interes_anual),
+                costo_total = string.Format("RD {0:C}", res_total),
+                costo_financiamiento = string.Format("RD {0:C}", costo_fin)
+            });
+        }
     }
 }
--- SisPrestamo/Controllers/HomeController.cs
+++ SisPrestamo/Controllers/HomeController.cs
@@ -10,21 +10,21 @@ namespace SisPrestamo.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "";
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Presta+ fue desarrollado por Martin E. De Leon y Efrain Toribio ";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contactanos";
 
             return View();
         }
